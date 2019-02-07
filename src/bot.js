ef4ffd15d5e9645f5be35a8fda81e5926e6d9895
RaymondW <tnthacker2015@gmail.com>
2019-02-06 19:48:26 -0800

    Initial

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,56 @@
 const Client = require("./structs/client.struct");
+const client = new Client();
+const Discord = require("discord.js");
 const auth = require("./auth");
-Client.login(auth.token);
+const chalk = require("chalk");
+const strings = require("./modules/strings");
+const { models: { guildinfo }, models, prefix: defaultPrefix } = require("./modules/sql");
+const { errors } = strings;
+Object.defineProperty(Error.prototype, "short", {
+	get() {
+		return `${this.name}: ${this.message}`;
+	}
+});
+Object.defineProperty(Error.prototype, "shortcolors", {
+	get() {
+		return `${chalk.redBright(this.name)}: ${chalk.red(this.message)}`;
+	}
+});
+client.log("Starting bot...");
+client.on("ready", async() => {
+	client.error(`${chalk.blue("Shard 0")} Failed to Load.`)
+	client.warn(`${chalk.yellow("Warning")}: asdf`)
+	for (const [mname, model] of Object.entries(models)) {
+		try {
+			await model.sync({ alter: true });
+		} catch (err) {
+			client.error(`Model ${mname} failed to load. Reason: ${chalk.red(err.short)}`);
+		}
+	}
+	client.log(`${chalk.cyanBright("Bot started!")} Logged in at ${chalk.bold(client.user.tag)}. ID: ${chalk.blue(client.user.id)}`);
+});
+client.on("message", async message => {
+	if (message.author.bot) return;
+	message.guild.info = await guildinfo.findOrCreate({ where: { id: message.guild.id }, defaults: { id: message.guild.id } });
+	const prefixes = [defaultPrefix, `<@${client.user.id}>`, `<@!${client.user.id}>`, message.guild.info.prefix];
+	const prefix = prefixes.find(x => message.content.startsWith(x));
+	if (!prefix) return;
+	message.content = message.content.replace(prefix, "").trim();
+	const args = message.content.split(/\s+/);
+	const command = args.shift();
+	try {
+		client.getCommand(command).exec(client, message, args);
+	} catch (err) {
+		await message.channel.send(`${errors.internal}
+\`\`\`js
+${err.stack}
+\`\`\`
+		`);
+		client.error(err);
+	}
+});
+process.on("unhandledRejection", (err, p) => {
+	client.error(err.shortcolors);
+});
+
+client.login(auth.token);
--- src/structs/client.struct.js
+++ src/structs/client.struct.js
@@ -1,13 +1,35 @@
-const { Client } = require("discord.js");
+const { Client, Collection } = require("discord.js");
 const glob = require("glob");
+const moment = require("moment-timezone");
+const chalk = require("chalk");
+const { timestamp } = require("../modules/util");
+const { compareTwoStrings } = require("string-similarity");
 module.exports = class DiscordDonuts extends Client {
 	constructor() {
 		super({ disableEveryone: true });
+		this.commands = new Collection();
+		this.loadCommands();
 	}
 	loadCommands() {
-		const commandFiles = glob.sync("./src/commands/**/*.js");
-		for (const file of commandFiles) {
-			console.log(file);
+		const commandFiles = glob.sync("./src/commands/**/*.js").map(file => require(`../.${file}`));
+		for (const command of commandFiles) {
+			this.commands.set(command.name, command);
+			this.log(`Command ${chalk.magentaBright(command.name)} loaded!`);
 		}
+	}
+	timestamp() {
+		return `[${timestamp()}]`;
+	}
+	log(str) {
+		console.log(`${chalk.gray(this.timestamp())} ${str}`);
+	}
+	warn(str) {
+		console.warn(`${chalk.yellowBright(this.timestamp())} ${str}`);
+	}
+	error(str) {
+		console.error(`${chalk.redBright(this.timestamp())} ${str}`);
+	}
+	getCommand(commandResolvable) {
+		return this.commands.find(command => [command.aliases, command.shortcuts, command.name].some(str => str === commandResolvable || (compareTwoStrings(str, commandResolvable) > 0.75))) || null;
 	}
 };
