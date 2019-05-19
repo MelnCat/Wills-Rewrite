44d525183c3e4f8dd4160a199a18bd68aabfc6ec
AdrianBerger <ilonabe1978@gmail.com>
2019-05-19 14:54:10 +0300

    blacklist command, see if there's any bugs
    ^

---
+++ src/commands/debugging/blacklist.cmd.js
@@ -0,0 +1,26 @@
+const Command = require("../../structs/command.struct");
+
+
+module.exports = new Command("blacklist", "Blacklist a user.", 4)
+    .setFunction(async(client, message, args) => {
+        const blacklist = getModel("blacklists")
+        const { Op } = getModule("sql")
+
+
+            let id = args[0];
+            //if(isNaN(id)) return; //message todo
+            if(!id) return; //message todo
+
+            let b;
+            try{
+                await blacklist.findByPk(id)
+            }catch(err) {
+                //do nothing, this is just to see if the user is already blacklisted
+            }
+            if(b !== undefined) return; //message todo
+
+            blacklist.create({ id })
+            //success: todo
+    })
+    
+    .setAlias("bl");
