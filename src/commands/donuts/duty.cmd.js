9103a7ed194e7e952afaa1ba900556c6caf02abf
TNThacker2015 <37024464+TNThacker2015@users.noreply.github.com>
2019-05-19 12:06:25 -0700

    Update duty.cmd.js

--- src/commands/donuts/duty.cmd.js
+++ src/commands/donuts/duty.cmd.js
@@ -2,32 +2,16 @@ const Command = require("../../structs/command.struct");
 module.exports = new Command("duty", "Go on or off duty.", 2);
     .setFunction(async( client, message ) => {
 
-        let user = message.member;
-        let r = client.roles.onduty
+        const onduty = client.roles.onduty
 
-        user.roles.exists("id", r) ? user.roles.remove("id", r) : user.roles.add("id", r);
+        if (message.member.roles.has(onduty)) {
+            await message.member.roles.remove("id", onduty)
+            await message.reply(`You're no longer on ${message.command.inputName}.`)
+        } else {
+            await message.member.roles.add("id", onduty);
+            await message.reply(`You're now on ${message.command.inputName}!`)
+        }
         
-        if(message.content.includes("d!duty")){
-            message.reply("You're now on duty!")
-        }
-        if(message.content.includes("d!dudey")){
-            message.reply("You're now on dudey!")
-        }
-        if(message.content.includes("d!doody")){
-            message.reply("You're now on doody!")
-        }
-        if(message.content.includes("d!dowdey")){
-            message.reply("You're now on dowdey!")
-        }
-        if(message.content.includes("d!dooooooty")){
-            message.reply("You're now on dooooooty!")
-        }
-        if(message.content.includes("d!dooty")){
-            message.reply("You're now on dooty!")
-        }
-        if(message.content.includes("d!dudy")){
-            message.reply("You're now on dudy!")
-        }
 
     });
 
