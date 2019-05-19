4edd2a655a82ff983333dc0ff41b7d3e68953a25
Your Name <you@example.com>
2019-05-19 12:09:56 -0700

    fixes

--- src/commands/donuts/duty.cmd.js
+++ src/commands/donuts/duty.cmd.js
@@ -1,13 +1,13 @@
 const Command = require("../../structs/command.struct");
 module.exports = new Command("duty", "Go on or off duty.", 2)
 	.setFunction(async(client, message) => {
-		const onduty = client.roles.onduty;
+		const onduty = client.strings.roles.onduty;
 
 		if (message.member.roles.has(onduty)) {
-			await message.member.roles.remove("id", onduty);
+			await message.member.roles.remove(onduty);
 			await message.reply(`You're no longer on ${message.command.inputName}.`);
 		} else {
-			await message.member.roles.add("id", onduty);
+			await message.member.roles.add(onduty);
 			await message.reply(`You're now on ${message.command.inputName}!`);
 		}
 	}).setAlias("dudey", "dooty");
