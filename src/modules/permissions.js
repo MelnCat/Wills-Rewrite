d04b6eb6004bec6e548f60e96e48fd9cee9de2c2
Your Name <you@example.com>
2019-05-19 11:34:15 -0700

    William: Premissions

--- src/modules/permissions.js
+++ src/modules/permissions.js
@@ -1,8 +1,8 @@
 class Permissions extends Array {}
 const permissions = new Permissions;
 permissions[0] = () => true;
-permissions[1] = (client, member) => client.getMainMember(member.id) ? client.getMainMember(member.id).roles.has(client.mainRoles.employee.id) : false;
-permissions[2] = (client, member) => member.permissions.has("MANAGE_GUILD");
+permissions[1] = (client, member) => member.permissions.has("MANAGE_GUILD");
+permissions[2] = (client, member) => client.getMainMember(member.id) ? client.getMainMember(member.id).roles.has(client.mainRoles.employee.id) : false;
 permissions[3] = (client, member) => client.getMainMember(member.id) ? client.getMainMember(member.id).permissions.has("MANAGE_MESSAGES") : false;
 permissions[4] = (client, member) => client.auth.botOwners.includes(member.id);
 module.exports = permissions;
