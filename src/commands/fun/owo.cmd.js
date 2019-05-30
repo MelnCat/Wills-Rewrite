ad611857bdaa2cd2853f6524a47801cbab703cfc
TNThacker2015 <37024464+TNThacker2015@users.noreply.github.com>
2019-05-29 21:20:16 -0700

    Update owo.cmd.js

--- src/commands/fun/owo.cmd.js
+++ src/commands/fun/owo.cmd.js
@@ -2,5 +2,5 @@ const owofy = require('owofy');
 
 const Command = require("../../structs/command.struct");
 
-module.exports = new Command("owo", "uwu owo >.<", 0)
+module.exports = new Command("owo", "uwu", 0)
 	.setFunction(async(client, message, args) => message.channel.send(owofy(args.join(" "))));
