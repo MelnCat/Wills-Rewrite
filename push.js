e136ea6d399ba98c95187d62186a5a843ae5a11f
Beta <21072460+bvta@users.noreply.github.com>
2019-06-23 12:14:41 -0500

    clean

--- push.js
+++ push.js
@@ -1,10 +1,14 @@
 const { exec } = require("child_process");
 const { promisify } = require("util");
+
 (async() => {
 	const arg = process.argv.slice(2).join(" ");
 	if (!arg) return console.log("No argument!");
+
 	console.log(`Pushing...`);
+
 	const functions = ["git add -A", `git commit -m "${arg}"`, "git pull", "git push"];
+	
 	for (const f of functions) {
 		try {
 			const res = await promisify(exec)(f);
