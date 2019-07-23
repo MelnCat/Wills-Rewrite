55507c69cd0206154a2a8f84f301e30a0adcbf30
Your Name <you@example.com>
2019-07-22 17:21:38 -0700

    fixx

--- src/commands/debugging/pull.cmd.js
+++ src/commands/debugging/pull.cmd.js
@@ -4,7 +4,7 @@ const { exec } = require("child_process");
 const p_exec = promisify(exec);
 module.exports = new Command("pull", "Pull changes to the bot.", "", 4)
 	.setFunction(async(client, message, args, strings) => {
-		const list = ["git add - A", "git commit - m 'Pulling'", "git pull"];
-		const res = await Promise.all(list.map(x => `[${x}] ${client.utils.execBash(x)}`));
-		await message.channel.send(res.join("\n\n"), { code: "ini" });
+		const list = ["git add -A", "git commit -m 'Pulling'", "git pull"];
+		const res = await Promise.all(list.map(async x => `[${x}]\n${await client.utils.execBash(x)}`));
+		await message.channel.send(res.join("\n"), { code: "ini" });
 	});
