ac8f451e1549f398d772bddd4209b2231441e922
Your Name <you@example.com>
2019-07-22 17:08:35 -0700

    poull comanhd

--- src/commands/debugging/pull.cmd.js
+++ src/commands/debugging/pull.cmd.js
@@ -3,4 +3,8 @@ const { promisify } = require("util");
 const { exec } = require("child_process");
 const p_exec = promisify(exec);
 module.exports = new Command("pull", "Pull changes to the bot.", "", 4)
-	.setFunction(async(client, message, args, strings) => message.channel.send(await client.utils.execBash("git add -A;git commit -m 'Pulling';git pull"), { code: "bash" }));
+	.setFunction(async(client, message, args, strings) => {
+		const list = ["git add - A", "git commit - m 'Pulling'", "git pull"];
+		const res = Promise.all(list.map(x => client.utils.execBash(x)));
+		await message.channel.send(res.join("\n\n"), { code: "bash" });
+	});
