fca27ff283c96eee07f91f8f95b91f3bc7ee17c1
Your Name <you@example.com>
2019-07-25 14:27:03 -0700

    aaaaa

--- src/website/server.js
+++ src/website/server.js
@@ -16,7 +16,25 @@ const nav = require("./navigation");
 hbs.registerHelper("ulist", arr => new hbs.SafeString(`<ul>\n${arr.map(x => `	<li>${x}</li>`).join("\n")}\n</ul>`));
 hbs.registerHelper("icon", str => new hbs.SafeString(`<i class="fas fa-${str}"></i>`));
 // partials
-hbs.registerPartial("header", `<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script><script src="https://kit.fontawesome.com/bfe4c30235.js"></script><style>${globalStyle}</style><script>document.addEventListener("DOMContentLoaded", async function() {${globalScript} }, false);</script>`);
+hbs.registerPartial("header", `
+<link id="favicon" rel="icon" href="${pagify("./public/images/icon.ico")}" type="image/x-icon">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://kit.fontawesome.com/bfe4c30235.js"></script>
+<style>${globalStyle}</style>
+<script>document.addEventListener("DOMContentLoaded", async function() {${globalScript} }, false);</script>
+<meta name="description" content="A discord bot where you can order donuts and get it delivered to your server!">
+<meta name="og:type" content="website">
+<meta name="og:url" content="">
+<meta name="og:title" content="Discord Donuts: Order donuts, get it delivered!">
+<meta name="og:description" content="Discord donuts is a bot where you can order donuts, and get it delivered to your server! What are you waiting for? Add the bot NOW!">
+<meta property="og:site_name" content="Discord Donuts">
+<meta name="og:image" content="./images/ddicon.png">
+<meta name="twitter:card" content="summary_large_image">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+`);
 hbs.registerPartial("starter", `
 <ul id="nav">
 ${nav.map(x => `<li><a href="${pagify(x.page)}" class=>${x.display}</a></li>`).join("\n")}
