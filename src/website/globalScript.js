96d7b58f042dbf5c3c6b8f696c9c30670810603f
Your Name <you@example.com>
2019-07-25 15:42:30 -0700

    aa

--- src/website/globalScript.js
+++ src/website/globalScript.js
@@ -1,5 +1,6 @@
 /* eslint-env browser */
 console.log("rady!");
+const navBar = docuemnt.getElementById("nav");
 const allElem = $("#nav").nextAll();
 const allDiv = document.createElement("DIV");
 allDiv.setAttribute("style", "margin-left:25%; padding:1px 16px; height:1000px");
@@ -9,3 +10,6 @@ $(`ul#nav li a`)
 	.toArray()
 	.find(x => document.location.href.match(`${x.href.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")}?$`))
 	.classList.add("active");
+setInterval(()=> {
+	allDiv.style.marginLeft = `${navBar.offsetWidth}px`
+}, 10)
