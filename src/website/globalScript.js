67fa960cdd344297744f681ad6430e6939eee4c6
Your Name <you@example.com>
2019-07-25 15:48:08 -0700

    fix

--- src/website/globalScript.js
+++ src/website/globalScript.js
@@ -1,6 +1,6 @@
 /* eslint-env browser */
 console.log("rady!");
-const navBar = docuemnt.getElementById("nav");
+const navBar = document.getElementById("nav");
 const allElem = $("#nav").nextAll();
 const allDiv = document.createElement("DIV");
 allDiv.setAttribute("style", "margin-left:25%; padding:1px 16px; height:1000px");
