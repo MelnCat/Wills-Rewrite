daeefff2bea13cddb93730c79023fde5630a8074
Your Name <you@example.com>
2019-07-24 17:50:53 -0700

    aaaa

--- src/website/server.js
+++ src/website/server.js
@@ -3,12 +3,14 @@ const hbs = require("hbs");
 const client = require("../modules/client");
 const chalk = require("chalk");
 const app = express();
-const server = app.listen(3000 /* set to 80 when done */, () => {
-	client.log(`Website started on ${chalk.greenBright(server.address().port)}!`);
-});
-app.set("view engine", "hbs");
-// GET ROUTES
-app.use(express.static("public"));
-app.get("/", (req, res) => {
-	res.send("test good");
-});
+(async() => {
+	const server = app.listen(42069, () => {
+		client.log(`Website started on ${chalk.greenBright(server.address().port)}!`);
+	});
+	app.set("view engine", "hbs");
+	// GET ROUTES
+	app.use(express.static("public"));
+	app.get("/", (req, res) => {
+		res.send("test good");
+	});
+})();
