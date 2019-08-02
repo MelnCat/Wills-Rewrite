4f8b2c3c7eb658758ea50910e437e7493bb5feaa
Your Name <you@example.com>
2019-08-02 09:37:54 -0700

    add

--- src/commands/stock/restock.cmd.js
+++ src/commands/stock/restock.cmd.js
@@ -4,9 +4,9 @@ const Command = require("../../structs/command.struct");
 module.exports = new Command("restock", "Restock an ingredient!", "", 2)
 	.setFunction(async(client, message, args, strings) => {
 		const ingredients = client.getModel("stocks");
-		const all = await ingredients.findAll();
 		const msg = await message.channel.send(`Loading ingredient matrix...`);
-		const stocks = new client.classes.Matrix(6, 3, () => all.random());
+		const all = await ingredients.findAll();
+		const stocks = new client.classes.Matrix(8, 5, () => all.random());
 		const display = client.classes.Matrix.from(stocks.map.map(y => y.map(x => x.emoji)));
 		await msg.edit(display.toString());
 	});
